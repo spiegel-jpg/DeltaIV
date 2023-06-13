6b2ce36f01701e89a3391515e48a6d05b8db7dcb
Darpan Shrivastava <86070515+spiegel-jpg@users.noreply.github.com>
2023-06-13 19:01:43 -0400

    Just added a print statement

--- downloaded_data.py
+++ downloaded_data.py
@@ -3,3 +3,4 @@ import requests
 url = "https://my.api.mockaroo.com/user_details.json?key=_______" # the key is removed for privacy reasons. 
 response = requests.get(url)
 print(response.status_code) 
+print(response.text)
